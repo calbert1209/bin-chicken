b88462873ca2f7823994ff7725cef2fa574f931d
Christian Albert <calbert1209@gmail.com>
2022-12-12 21:51:17 +0900

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const baseConfig = {
+    plugins: [react()],
+  };
+  if (command === "serve") {
+    return baseConfig;
+  } else {
+    return {
+      ...baseConfig,
+      base: "/bin-chicken/",
+    };
+  }
+});
