feefa7534c66aac1e47a2f52b5cad53b451b8085
Christian Albert <calbert1209@gmail.com>
2022-12-16 22:42:56 +0900

    add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const baseConfig = {
-    plugins: [react()],
+    plugins: [react(), svgr()],
   };
   if (command === "serve") {
     return baseConfig;
