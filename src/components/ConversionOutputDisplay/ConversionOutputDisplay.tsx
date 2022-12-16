d94bf8e696aa6f76dc18e41eb0755ff76b6034c4
Christian Albert <calbert1209@gmail.com>
2022-12-16 22:40:53 +0900

    add copy button

--- src/components/ConversionOutputDisplay/ConversionOutputDisplay.tsx
+++ src/components/ConversionOutputDisplay/ConversionOutputDisplay.tsx
@@ -3,10 +3,17 @@ import { FC } from "react";
 export const ConversionOutputDisplay: FC<{
   value: string;
   status: "result" | "error";
-}> = ({ value, status }) => (
-  <div>
-    <div id="output" data-status={status}>
-      {value}
+}> = ({ value, status }) => {
+  const handleOnClickCopy = () => navigator.clipboard.writeText(value);
+
+  return (
+    <div id="output-display">
+      <div id="output" data-status={status}>
+        {value}
+      </div>
+      <button id="copy-button" onClick={handleOnClickCopy}>
+        copy
+      </button>
     </div>
-  </div>
-);
+  );
+};
