1394971485778434164d83f610c19ecf6371a2bc
Christian Albert <calbert1209@gmail.com>
2022-12-25 20:38:25 +0900

    move old layout to own page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,8 @@
-import { useState } from "preact/hooks";
 import "./App.css";
-import {
-  ConversionForm,
-  ConversionPayload,
-  isConversionPayload,
-} from "./components/ConversionForm/ConversionForm";
-import { ConversionOutputDisplay } from "./components/ConversionOutputDisplay/ConversionOutputDisplay";
-import { decryptFromHex, encrypt2hex } from "./service/AES-GCM";
+import ThreeColumnPage from "./page/ThreeColumnPage";
 
 function App() {
-  const [convertedValue, setConvertedValue] = useState(
-    "I am not a cryptographer"
-  );
-  const [error, setError] = useState<Error | null>(null);
-
-  const handleOnSubmitEncrypt = (payload: Partial<ConversionPayload>) => {
-    if (isConversionPayload(payload)) {
-      const { password, iv, text } = payload;
-      encrypt2hex(password, iv, text)
-        .then((cypherText) => {
-          setConvertedValue(cypherText);
-          setError(null);
-        })
-        .catch((error) => {
-          setError(error);
-        });
-    }
-  };
-
-  const handleOnSubmitDecrypt = (payload: Partial<ConversionPayload>) => {
-    if (isConversionPayload(payload)) {
-      const { password, iv, text } = payload;
-      decryptFromHex(password, iv, text)
-        .then((plainText) => {
-          setConvertedValue(plainText);
-          setError(null);
-        })
-        .catch((error) => {
-          setError(error);
-        });
-    }
-  };
-
-  return (
-    <div className="App">
-      <h2>Bin Chicken</h2>
-      <div className="columns">
-        <ConversionForm
-          id="encrypt"
-          legend="encrypt"
-          actionLabel="encrypt →"
-          textAreaLabel="plain text"
-          onSubmit={handleOnSubmitEncrypt}
-        />
-        <ConversionOutputDisplay
-          value={error ? error.toString() : convertedValue}
-          status={error ? "error" : "result"}
-        />
-        <ConversionForm
-          id="decrypt"
-          legend="decrypt"
-          actionLabel="← decrypt"
-          textAreaLabel="cypher text"
-          onSubmit={handleOnSubmitDecrypt}
-        />
-      </div>
-    </div>
-  );
+  return <ThreeColumnPage />;
 }
 
 export default App;
