75dc10e11e9ef146f46a1b31f5c95ebd5bfc4c1f
Christian Albert <calbert1209@gmail.com>
2022-12-27 23:07:17 +0900

    parse form input values

--- src/components/EncryptTab/EncryptTab.tsx
+++ src/components/EncryptTab/EncryptTab.tsx
@@ -1,9 +1,27 @@
-import { useState } from "preact/hooks";
+import { useRef, useState } from "preact/hooks";
+import { JSXInternal } from "preact/src/jsx";
 import { Alert, AlertLevel } from "../common/Alert";
+import { SecretInput } from "../common/SecretInput";
 import "./EncryptTab.css";
 
 export function EncryptTab() {
   const [error, setError] = useState<Error | null>(null);
+  const secretRef = useRef<HTMLInputElement | null>(null);
+  const cypherIvRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const parseCypherIv = (value: string | undefined) => {
+    if (!value) return null;
+
+    try {
+      setError(null);
+      return JSON.parse(value);
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e);
+      }
+    }
+  };
+
   return (
     <div className="encrypt-tab">
       {error && (
@@ -11,7 +29,19 @@ export function EncryptTab() {
           {error.message}
         </Alert>
       )}
-      <div>encrypt form contents</div>
+      <SecretInput className="encrypt-secret-input" ref={secretRef} />
+      <textarea ref={cypherIvRef} />
+      <button
+        onClick={() =>
+          console.log("encrypt state", {
+            secret: secretRef.current?.value,
+            cypherIv: parseCypherIv(cypherIvRef.current?.value),
+            error,
+          })
+        }
+      >
+        encrypt
+      </button>
     </div>
   );
 }
--- src/components/common/SecretInput.tsx
+++ src/components/common/SecretInput.tsx
@@ -1,20 +1,23 @@
+import { forwardRef } from "preact/compat";
 import { useState } from "preact/hooks";
 import { JSXInternal } from "preact/src/jsx";
 import { HiddenIcon, VisibleIcon } from "../../assets/icons";
 import { IconButton } from "./IconButton";
 import "./SecretInput.css";
 
-export function SecretInput(
-  props: JSXInternal.HTMLAttributes<HTMLInputElement>
-) {
+export const SecretInput = forwardRef<
+  HTMLInputElement,
+  JSXInternal.HTMLAttributes<HTMLInputElement>
+>((props, ref) => {
   const [hidden, setHidden] = useState(true);
   const Icon = hidden ? HiddenIcon : VisibleIcon;
+
   return (
-    <div className="secret-input">
-      <input {...props} type={hidden ? "password" : "text"} />
+    <form className="secret-input">
+      <input {...props} ref={ref} type={hidden ? "password" : "text"} />
       <IconButton onClick={() => setHidden((s) => !s)}>
         <Icon />
       </IconButton>
-    </div>
+    </form>
   );
-}
+});
