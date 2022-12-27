f6d8f87264712e0091ccf4e4a786b5f5f552689a
Christian Albert <calbert1209@gmail.com>
2022-12-27 21:47:51 +0900

    add icon components

---
+++ src/assets/icons.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as CopyIcon } from "./copy-icon.svg";
+export { ReactComponent as CloseIcon } from "./close-icon.svg";
+export { ReactComponent as LockIcon } from "./lock-icon.svg";
+export { ReactComponent as UnlockIcon } from "./unlock-icon.svg";
---
+++ src/components/common/LabeledIcon.tsx
@@ -0,0 +1,17 @@
+import { ComponentChild } from "preact";
+import { FilledIcon } from "./FilledIcon";
+import "./LabeledIcon.css";
+
+interface LabeledIconProps {
+  label: string;
+  children: ComponentChild;
+}
+
+export function LabeledIcon({ label, children }: LabeledIconProps) {
+  return (
+    <div className="labeled-icon">
+      <div>{label}</div>
+      <FilledIcon>{children}</FilledIcon>
+    </div>
+  );
+}
