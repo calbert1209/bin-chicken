f1452a144bcff9ce1f5081dcd61f5bd1c47631df
Christian Albert <calbert1209@gmail.com>
2022-12-29 17:26:38 +0900

    remove unused page from App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import { TabbedPage } from "./page/TabbedPage";
-import ThreeColumnPage from "./page/ThreeColumnPage";
 
 function App() {
   return <TabbedPage />;
-  // return <ThreeColumnPage />;
 }
 
 export default App;
