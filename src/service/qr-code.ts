9ae89594820e878c3a3031992c470e0f4ecaad6b
Christian Albert <calbert1209@gmail.com>
2024-12-20 12:10:18 +0000

    add utility to generate qr code data url

---
+++ src/service/qr-code.ts
@@ -0,0 +1,15 @@
+// cspell:ignore qrcode
+import qrcode from "qrcode-generator";
+
+type TypeNumber = Parameters<typeof qrcode>[0];
+type ErrorCorrectionLevel = Parameters<typeof qrcode>[1];
+
+const TYPE: TypeNumber = 0;
+const CORRECTION_LEVEL: ErrorCorrectionLevel = "M";
+
+export function generateQRCodeDataUrl(content: string, size: number = 4) {
+  const qr = qrcode(TYPE, CORRECTION_LEVEL);
+  qr.addData(content);
+  qr.make();
+  return qr.createDataURL(size);
+}
